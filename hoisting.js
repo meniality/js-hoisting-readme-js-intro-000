7d20b7025958c16291a0a5c0c26b914ef1dd2d26
patrick hoehn <patrh_99@hotmail.com>
2019-09-29 01:04:21 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -11,11 +11,10 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -25,7 +24,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
